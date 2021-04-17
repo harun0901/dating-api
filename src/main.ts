dba5a8345a9a9052320e5fb15f4a5ebc7519e921
FranglinSeed <franglin38@gmail.com>
2021-04-17 16:01:21 +0300

    port setting

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('doc', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.API_PORT || process.env.PORT);
 }
 bootstrap();
